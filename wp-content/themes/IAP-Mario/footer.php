2f30e56b152966f64cf64f0a30ae6e81d5dfca72
Marian Horoiu <marian.horoiu@gmail.comgit>
2019-07-05 08:34:33 +0300

    Implemented dynamic display of copyright years in footer.php and function.php

--- wp-content/themes/IAP-Mario/footer.php
+++ wp-content/themes/IAP-Mario/footer.php
@@ -1,4 +1,16 @@
-        <p>Greetings from footer.php</p>
+        <footer id="site-footer" class="site-footer" role="contentinfo">
+            <div class="container">
+                <div class="row">
+                    <div class="cal-sm-12">
+                        <div class="footer-info">
+                            <?php echo(copyright_years()); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+
 
         <?php wp_footer(); ?>
     </body>
--- wp-content/themes/IAP-Mario/functions.php
+++ wp-content/themes/IAP-Mario/functions.php
@@ -1,7 +1,33 @@
 <?php 
 
+    /* Loads the main stylesheet file, styles.css */
     function blog_files() {
         wp_enqueue_style('blog_main_styles', get_stylesheet_uri());
     }
 
     add_action('wp_enqueue_scripts', 'blog_files');
+
+
+    /* Add footer copyright years; gets the year from first and last posts */
+    /* Function called from footer.php */
+    function copyright_years() {
+        global $wpdb;
+        $copyright_dates = $wpdb->get_results("
+            SELECT
+            YEAR(min(post_date_gmt)) AS firstdate,
+            YEAR(max(post_date_gmt)) AS lastdate
+            FROM
+            $wpdb->posts
+            WHERE
+            post_status = 'publish'
+            ");
+        $output = '';
+        if($copyright_dates) {
+            $copyright = "&copy; All rights reserved to Marian Horoiu, " . $copyright_dates[0]->firstdate;
+            if($copyright_dates[0]->firstdate != $copyright_dates[0]->lastdate) {
+                $copyright .= '-' . $copyright_dates[0]->lastdate;
+            }
+            $output = $copyright;
+        }
+        return $output;
+    }
